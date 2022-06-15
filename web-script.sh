396ef7673cadf56938c3de3a734398d2c3e9de70
Flavio Pereira <flaviosantino@gmail.com>
2022-06-15 11:49:29 -0500

    Update web-script.sh
    add OL8 php-fpm

--- web-script.sh
+++ web-script.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-sudo yum install httpd php -y
+sudo yum install httpd php php-fpm -y
 sudo rm -f /etc/httpd/conf/httpd.conf
 sudo wget -P /etc/httpd/conf/ https://objectstorage.us-phoenix-1.oraclecloud.com/n/bmc-flaviop/b/web-page/o/httpd.conf
 sudo wget -P /var/www/html/ https://objectstorage.us-phoenix-1.oraclecloud.com/n/bmc-flaviop/b/web-page/o/index.php
+sudo systemctl start php-fpm
+sudo systemctl enable php-fpm
 sudo apachectl start
 sudo systemctl enable httpd
 sudo firewall-offline-cmd --add-port=80/tcp

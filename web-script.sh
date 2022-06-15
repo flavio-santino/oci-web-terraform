75c0a0b651416dc1d2623e95f527b3ab8e621bb5
Flavio Pereira <flaviosantino@gmail.com>
2022-06-15 12:09:19 -0500

    Update web-script.sh
    Added OL8 PHP packages

--- web-script.sh
+++ web-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo yum install httpd php php-fpm -y
+sudo yum install httpd php php-fpm php-zip php-gd php-mbstring php-xml php-json -y 
 sudo rm -f /etc/httpd/conf/httpd.conf
 sudo wget -P /etc/httpd/conf/ https://objectstorage.us-phoenix-1.oraclecloud.com/n/bmc-flaviop/b/web-page/o/httpd.conf
 sudo wget -P /var/www/html/ https://objectstorage.us-phoenix-1.oraclecloud.com/n/bmc-flaviop/b/web-page/o/index.php

ea0b1daa5bd236dca171f205d92e70a028b6941a
Flavio Pereira <flaviosantino@gmail.com>
2022-06-16 09:36:33 -0500

    Update web-script.sh
    Fixed script for OL8

--- web-script.sh
+++ web-script.sh
@@ -6,6 +6,8 @@ sudo wget -P /etc/httpd/conf/ https://objectstorage.us-phoenix-1.oraclecloud.com
 sudo wget -P /var/www/html/ https://objectstorage.us-phoenix-1.oraclecloud.com/n/bmc-flaviop/b/web-page/o/index.php
 sudo systemctl start php-fpm
 sudo systemctl enable php-fpm
+sudo setsebool -P httpd_can_network_connect 1
+sudo setsebool -P httpd_unified 1
 sudo apachectl start
 sudo systemctl enable httpd
 sudo firewall-offline-cmd --add-port=80/tcp
